ea0d87eba0999266084a2062f7c6fcc3806ed03b
toshi <toshi.mana.plus1@gmail.com>
2013-08-12 02:34:23 -0700

    撮影画像をSDカードに保存できるように変更

--- src/jp/tmanabe/matsurikioku/CameraView.java
+++ src/jp/tmanabe/matsurikioku/CameraView.java
@@ -1,8 +1,22 @@
 package jp.tmanabe.matsurikioku;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Bitmap.CompressFormat;
 import android.hardware.Camera;
 import android.hardware.Camera.CameraInfo;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -62,11 +76,54 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback, C
 	}
 	
 	public void onPictureTaken(byte[] data, Camera camera) {
-	try	{	
-		// do nothing.
+	try	{
+		final String saveDir = Environment.getExternalStorageDirectory().getPath() + "/MatsuriKioku";
+		File file = new File(saveDir);
+		if (!file.exists()) {
+			if (!file.mkdir()) {
+				Log.e("Debug", "MakeDir Error");
+			}
+		}
+		
+		// convert byteArray to Bitmap.	
+		Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+
+		// create filepath.
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+		String imgPath = saveDir + '/' + sdf.format(cal.getTime()) + ".jpg";
+		
+		FileOutputStream fos;
+		try {
+			// second argment is that file is appended or not.
+			fos = new FileOutputStream(imgPath, true);
+			bitmap.compress(CompressFormat.JPEG, 100, fos);
+			fos.close();
+			
+			
+		} catch (Exception e) {
+			Log.e("Error", "" + e.toString());
+		}
+		
 	} catch (Exception e) { 
-	// do nothing.	
+		// do nothing.	
 	}
 	camera.startPreview();	
 	}
+
+	/**
+	 * アンドロイドのデータベースへ画像のパスを登録
+	 * @param path 登録するパス
+	 * http://androidguide.nomaki.jp/html/device/camera/camFileSave.html
+	 */
+	private void registAndroidDB(String path) {
+	    // アンドロイドのデータベースへ登録
+	    // (登録しないとギャラリーなどにすぐに反映されないため)
+	    ContentValues values = new ContentValues();
+	    ContentResolver contentResolver = this.getContext().getContentResolver();
+	    values.put(Images.Media.MIME_TYPE, "image/jpeg");
+	    values.put("_data", path);
+	    contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+	}
 }
+
